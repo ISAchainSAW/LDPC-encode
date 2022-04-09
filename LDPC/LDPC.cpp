bb882709535058bd864d0181bee054f69560ebc9
asin.gorbunov@yandex.ru <asin.gorbunov@yandex.ru>
2022-04-09 18:49:03 +0300

    AGA

--- LDPC/LDPC.cpp
+++ LDPC/LDPC.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <fstream>
-#define debug
 
 using namespace std;
 void fillArray(int** arr, int rows, int cols, int one, int& pop_one, int second, int & inf); //Функция заполнения разряженной проверочной матрицы
@@ -15,6 +14,7 @@ void SWAP_COLS(int** arr, int rows, int cols, int* H_swap);//Замена сто
 int one_chek(int** arr, int rows); //Функция проверки единиц по диагонали
 int one_chek(int** arr, int rows, int step); //Функция проверки единиц по диагонали с указанием изначального столбца
 void Sort(int** arr, int rc, int cols, int* H_swap, int rows);  //Переставляет столбцы по матрице перестановок
+void Cycle(int** arr, int rows, int cols);
 
 //read_me найти НОД чисел, потом поделить оба числа на этот НОД, получим кол-во единиц в строчках и столбцах соответственно
 //Восстановил неодинаковость строк 
@@ -25,22 +25,22 @@ int main()
     obj.open("Matrix.txt", fstream::out | fstream::app);
     if (!obj.is_open())
     {
-        cout << "Open ERROR\n";
+        std::cout << "Open ERROR\n";
     }
     int rows = 0;//Length message word (K)
     int cols = 0;//Length code word + parity matrix (N)
     int one = 0, pop_one = 0, second = 0;
     int repeat = 1; //Количество повторов выполнения программы
     int inf = 0;
-    cout << "Input rows = K = length message\nAnd input cols = N = length code word\n\nrows<cols\n\n";
-    cout << "rows= "; cin >> rows;
-    cout << "cols= "; cin >> cols;
-    cout << "one for rows= "; cin >> second;
-    cout << "one for cols= "; cin >> one;
+    std::cout << "Input rows = K = length message\nAnd input cols = N = length code word\n\nrows<cols\n\n";
+    std::cout << "rows= "; cin >> rows;
+    std::cout << "cols= "; cin >> cols;
+    std::cout << "one for rows= "; cin >> second;
+    std::cout << "one for cols= "; cin >> one;
     int rc = cols - rows;//Length rows H matrix
     if (cols <= rows)
     {
-        cout << "\n\nError input\n\n";
+        std::cout << "\n\nError input\n\n";
         return -1;
     }
     while (repeat!= 0) {
@@ -52,6 +52,7 @@ int main()
         int** H = new int* [rc];//Origainal mass
         int** HH = new int* [rc];//Copy original
         int** HHH = new int* [rc];//After sort mass
+
         //Fill arr pointer
         for (int i = 0; i < rc; i++)
         {
@@ -60,8 +61,12 @@ int main()
             HHH[i] = new int[cols] {};
         }
         fillArray(H, rc, cols, one, pop_one, second, inf);
+        //std::cout << "\nH matrix\n";
+        //showArray(H, rc, cols);
+
         if (inf == 150)
         {
+            std::cout << "\nError infinity\n";
             for (int i = 0; i < rc; i++)
             {
                 delete[] H[i];
@@ -72,8 +77,14 @@ int main()
             delete[] HH;
             delete[] HHH;
             inf = 0;
+            system("cls");
             continue;
         }
+
+        //=============================================================================<<<<< 
+        //ЦИКЛЫ----------------------------------------------------------------------------------
+        //Cycle(H, rc, cols);
+
         //Copy matrix
         for (int i = 0; i < rc; i++)
         {
@@ -83,25 +94,16 @@ int main()
             }
         }
         Gaus(H, rc, cols);
-        //cout << "\nAfter Gaus\n";
-        //showArray(H, rc, cols);
         if (one_chek(H, rc) < rc) // Проверка единиц по главной диагонали единичной матрицы
         {
             SWAP_COLS(H, rc, cols, H_swap);
-            //cout << "\nAfter Swap\n";
-            //showArray(H, rc, cols);
-            if (one_chek(H, rc) == rc) {
-                Gaus(H, rc, cols);
-                 //cout << "\nAfter Gaus_again\n";
-                 //showArray(H, rc, cols);
-                if (one_chek(H, rc) < rc) {
-                    SWAP_COLS(H, rc, cols, H_swap);
-                    //cout << "\nAfter Swap\n";
-                    //showArray(H, rc, cols);
-                }
-            }
+             if (one_chek(H, rc) == rc) {
+             Gaus(H, rc, cols);
+                 if (one_chek(H, rc) < rc) 
+                     SWAP_COLS(H, rc, cols, H_swap);   
+             }
             if (one_chek(H, rc) < rc) {
-                cout << "\nBad Generation\n";
+                std::cout << "\nBad Generation_1\n";
                 for (int i = 0; i < rc; i++)
                 {
                     delete[] H[i];
@@ -115,13 +117,9 @@ int main()
             }
         }
         Gaus_UP(H, rc, cols);
-        // cout << "\nAfter Gaus_UP\n";
-         //showArray(H, rc, cols);
         Sort(H, rc, cols, H_swap, rows);
-        //cout << "\nAfter Sort\n";
-        //showArray(H, rc, cols);
         if (one_chek(H, rc, rows) < rc) {
-            cout << "\nBad Generation\n";
+            std::cout << "\nBad Generation_2\n";
             for (int i = 0; i < rc; i++)
             {
                 delete[] H[i];
@@ -163,7 +161,7 @@ int main()
             r = 0;
         }
 
-        cout << "G matrix\n";
+        std::cout << "G matrix\n";
         showArray(Garr, rows, cols);
 
         for (int k = 0; k < cols; k++)
@@ -273,10 +271,10 @@ void Sort(int** arr, int rc, int cols, int* H_swap, int rows)
         }
         m++;
     }
-    //cout << "\n";
+    //std::cout << "\n";
     //for (int i = 0; i < cols; i++)
     //{
-    //    cout << "idx =" << i << "\t" << H_swap[i] << "\t" << H_swap2[i] << "\n";
+    //    std::cout << "idx =" << i << "\t" << H_swap[i] << "\t" << H_swap2[i] << "\n";
     //}
     for (int i = 0; i < rc; i++)
     {
@@ -319,14 +317,14 @@ void SWAP_COLS(int** arr, int rows, int cols, int* H_swap)
             //Поиск единиц по диагонали
             if (arr[i][j] != 1)
             {
-                //cout << "NeRAVNO\t" << i << "\t" << j << "\n";
+                //std::cout << "NeRAVNO\t" << i << "\t" << j << "\n";
                 //Поиск подходящего столбца на замену
                 for (int m = cols - 1; m > i + 1; m--)
                 {
                     if (arr[i][m] == 1)
                     {
                         
-                        //cout << "Found = "<< m <<"\n";
+                        //std::cout << "Found = "<< m <<"\n";
                         int tmp_swap = 0; // переменная обмена
                         //Записываем обмен столбцов в матрицу перемещений
                         tmp_swap = H_swap[m];
@@ -342,7 +340,7 @@ void SWAP_COLS(int** arr, int rows, int cols, int* H_swap)
                         }
                         break;
                     }
-                    //cout << "m= " << m<<"\n";
+                    //std::cout << "m= " << m<<"\n";
                 }         
             }
             break;
@@ -407,14 +405,14 @@ void Gaus_UP(int** arr, int rows, int cols)
 }
 void showArray(int** arr, int cols) 
 {
-    cout << "\t";
+    std::cout << "\t";
     for (int i = 0; i < 1; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            cout << arr[i][j] << "\t";
+            std::cout << arr[i][j] << "\t";
         }
-        cout << "\n";
+        std::cout << "\n";
     }
 }
 void showArray(int** arr, int rows, int cols)//Функция отображения массива на экран
@@ -423,24 +421,24 @@ void showArray(int** arr, int rows, int cols)//Функция отображен
     {
         for (int j = 0; j < cols; j++)
         {
-            cout << arr[i][j] << "\t";
+            std::cout << arr[i][j] << "\t";
         }
-        cout << "\n";
+        std::cout << "\n";
     }
 }
 void showArray(int** arr, int rows, int cols, int** arr2)
 {
     for (int i = 0; i < rows; i++)
     {
-        cout << arr2[0][i] << "\t";
+        std::cout << arr2[0][i] << "\t";
         for (int j = 0; j < cols; j++)
         {
-            cout << arr[i][j] << "\t";
+            std::cout << arr[i][j] << "\t";
         }
-        cout << "\n";
+        std::cout << "\n";
     }
 }
-void fillArray(int** arr, int rows, int cols, int one, int& pop_one, int second, int &inf) 
+void fillArray(int** arr, int rows, int cols, int one, int& pop_one, int second  , int& inf)
 {
     //oneR[0] for cols //ones in cols must be  == one           //walk by cols
     //oneC[1] for rows // ones in rows must be < 2              //walk by rows
@@ -461,8 +459,6 @@ void fillArray(int** arr, int rows, int cols, int one, int& pop_one, int second,
 
             if (arr[i][j] != 1) {
                 if (oneR[0][j] == one) continue;
-               // if (notzero(oneR, 1, cols, sign, j))//Проверка на пустые столбики, которые заполняем в первую очередь
-               //     continue;/////////////////////////////////////////////////////////////////////////////////
                 else arr[i][j] = rar(second, cols, pop_one);
                 if (arr[i][j] == 1)
                 {
@@ -470,10 +466,7 @@ void fillArray(int** arr, int rows, int cols, int one, int& pop_one, int second,
                     oneC[0][i]++;
                 }
             }
-            //showArray(oneR, cols);
-            //showArray(arr, rows, cols, oneC);
-            //cout << "________________________\n";
-            //Кол-во единиц в строчке
+            //Кол-во единиц в строчке и столбце не превышает значений
             if (oneC[0][i] > second)
             {
                 arr[i][j] = 0;
@@ -491,33 +484,87 @@ void fillArray(int** arr, int rows, int cols, int one, int& pop_one, int second,
         }
         if (oneC[0][i] == second) {
             pop_one = 0;
-            //----------------------------------------------------------------------------------------------------
+            //Неодинаковость пары строк
+           
+            if (i > 0) //Проверка начинается со 2 строчки
+            {
+                for (int p = 0; p < i; p++)//Цикл строк
+                {
+                    for (int f = 0; f < cols; f++)//Цикл столбцов
+                    {
+                        if (arr[i][f] == 1 && arr[p][f] == 1)//Проверка пересечения единиц в строках
+                        {
+                            chek_one++;
+                        }
+                    }
+                    if (chek_one > 1)//проверка на пересечение единиц в строках
+                    {
+                        cout << "p= " << p << "\n";
+                        for (int g = 0; g < cols; g++)
+                        {
+                            if (arr[i][g] == 1)
+                            {
+                                oneR[0][g]--;
+                                oneC[0][i]--;
+                            }
+                            arr[i][g] = 0;
+                        }
+                    }
+                    chek_one = 0;
+                }
+            }
         }
         if (oneC[0][i] != second) //Условие повторной генерации единиц в строках
         {
             inf++;
             i--;
-            if (inf==150)
+            if (inf == 150)
             {
                 pop_one = 0;
                 break;
             }
         }
+        //std::cout << "\nH matrix\n";
+        //showArray(oneR, cols);
+        //showArray(arr, rows, cols, oneC);
+        //std::cout << "________________________\n";
     }
-    cout << "\nH matrix\n";
-    showArray(oneR, cols);
-    showArray(arr, rows, cols, oneC);
-    cout << "________________________\n";
 }
 int rar(int one, const int cols, int& pop_one) 
 {
     int res = 0;
     float rand_v = rand() % 101 / static_cast<float>(100);
     float p = (static_cast<float>(one) - pop_one) / (static_cast<float>(cols) - pop_one);
-    if (rand_v < p)// <
+    if (rand_v < p)
     {
         res = 1;
         pop_one++;
     }
     return res;
+}
+void Cycle (int** arr, int rows, int cols)
+{
+    int** mas = new int*[rows];
+    int size_s = 0;
+    int k = 0, tmp = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (arr[i][j] == 1) size_s++;
+        }
+        mas[i] = new int[size_s];
+        for (int j = 0; j < cols; j++)
+        {
+            if (arr[i][j] == 1) {
+                mas[i][k] = j;
+                    k++;
+            }
+        }
+        k = 0;
+        tmp = size_s;
+        size_s = 0;
+    }   
+    cout << "\n";
+    showArray(mas, rows, tmp);
 }
